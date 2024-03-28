efc300b5508d2a47ce7e34ae56a94cd545ea19c1
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-03-28 16:01:45 +0000

    commit 5

--- Utilisateur/urls.py
+++ Utilisateur/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
 
-from Utilisateur.views import Inscription, Connexion_utlisateur
+from Utilisateur.views import Inscription, Connexion_utlisateur, accueil_utilisateur
 
 app_name = 'utilisateur'
 
 urlpatterns = [
     path('', Inscription, name='Inscription'),
     path('Connexion/', Connexion_utlisateur.as_view(), name='Connexion_utlisateur'),
+    path('accueil_utilisateur/', accueil_utilisateur, name='accueil_utilisateur'),
 ]
