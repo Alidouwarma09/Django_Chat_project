fdf4def79d199e2b4e9610a9191e128aa5999b24
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-03-28 14:41:21 +0000

    commit 4

--- Utilisateur/forms.py
+++ Utilisateur/forms.py
@@ -1,25 +1,13 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from Model.models import Utilisateur
 
 
 class InscriptionForm(UserCreationForm):
-
     class Meta(UserCreationForm.Meta):
         model = Utilisateur
-        fields = ['username', 'image', 'nom', 'prenom', 'password1', 'password2']
-
-        def clean(self):
-            cleaned_data = super().clean()
-
-            password1 = cleaned_data.get("password1")
-            password2 = cleaned_data.get("password2")
-
-            if password1 and password2 and password1 != password2:
-                self.add_error('password2', "Les deux mots de passe ne correspondent pas.")
-
-            return cleaned_data
+        fields = ['username', 'nom', 'prenom', 'image']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -38,3 +26,29 @@ class InscriptionForm(UserCreationForm):
             'id': "password2",
             'required': True,
         })
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get("password1")
+        password2 = cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Les deux mots de passe ne correspondent pas.")
+        return cleaned_data
+
+
+class ConnexionForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(ConnexionForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].error_messages = {
+            'required': "Veuillez saisir un Nom d'utilisateur valide !!"
+        }
+        self.fields['password'].error_messages = {
+            'required': "Veuillez saisir un mot de passe valide !!"
+        }
+        self.error_messages = {
+            "invalid_login":
+                "Veuillez saisir les mêmes informations que lors de la création de votre compte. "
+                "Vous devez respecter les majuscules ou les minuscules !!",
+            "inactive": "Ce compte est inactif veuillez contacter votre administrateur."
+        }
--- Utilisateur/urls.py
+++ Utilisateur/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
 
-from Utilisateur.views import Inscription
+from Utilisateur.views import Inscription, Connexion_utlisateur
 
 app_name = 'utilisateur'
 
 urlpatterns = [
-    path('', Inscription.as_view(), name='Inscription'),
+    path('', Inscription, name='Inscription'),
+    path('Connexion/', Connexion_utlisateur.as_view(), name='Connexion_utlisateur'),
 ]
