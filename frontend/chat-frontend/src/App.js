b56b0e5b0787abfecdcc14434f77c99c6fa4750a
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-05-29 06:34:04 +0000

    216

--- frontend/chat-frontend/src/App.js
+++ frontend/chat-frontend/src/App.js
@@ -8,42 +8,12 @@ import Videos from "./pages/Videos";
 import Messages from "./pages/Message";
 import { VideoProvider } from './compoment/VideoContext';
 import Utilisateurs from "./pages/Utilisateurs";
-import { useEffect } from "react";
 
 const isAuthenticated = () => {
     return localStorage.getItem('token') !== null;
 };
 
 function App() {
-    useEffect(() => {
-        const goFullScreen = () => {
-            if (document.documentElement.requestFullscreen) {
-                document.documentElement.requestFullscreen().catch((err) => {
-                    console.error(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-                });
-            } else if (document.documentElement.mozRequestFullScreen) { // Firefox
-                document.documentElement.mozRequestFullScreen().catch((err) => {
-                    console.error(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-                });
-            } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
-                document.documentElement.webkitRequestFullscreen().catch((err) => {
-                    console.error(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-                });
-            } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
-                document.documentElement.msRequestFullscreen().catch((err) => {
-                    console.error(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
-                });
-            }
-        };
-
-        // Instead of calling goFullScreen immediately, you can call it in response to a user action.
-        document.addEventListener('click', goFullScreen, { once: true });
-
-        return () => {
-            document.removeEventListener('click', goFullScreen);
-        };
-    }, []);
-
     return (
         <VideoProvider>
             <Router>
