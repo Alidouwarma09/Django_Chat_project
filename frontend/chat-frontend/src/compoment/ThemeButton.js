b45d4de19c7abee187be3a9f6ed18d1aaef9e71a
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-06-05 13:46:17 +0000

    235

--- frontend/chat-frontend/src/compoment/ThemeButton.js
+++ frontend/chat-frontend/src/compoment/ThemeButton.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { useColorMode, useColorModeValue, Button } from '@chakra-ui/react';
+import { useColorMode, Switch } from '@chakra-ui/react';
 
 function ThemeButton() {
     const { colorMode, toggleColorMode } = useColorMode();
-    const buttonBg = useColorModeValue('gray.300', 'gray.700');
 
     return (
-        <header>
-            <Button onClick={toggleColorMode} bg={buttonBg}>
-                Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
-            </Button>
-        </header>
+        <Switch
+            isChecked={colorMode === 'dark'}
+            onChange={toggleColorMode}
+            colorScheme="teal"
+        />
     );
 }
 
