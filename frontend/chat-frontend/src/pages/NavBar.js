b2c478ae0a92a225a5d0d3f52af2d5f8f3da46d6
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-06-11 12:04:52 +0000

    398

--- frontend/chat-frontend/src/pages/NavBar.js
+++ frontend/chat-frontend/src/pages/NavBar.js
@@ -308,6 +308,9 @@ function Navbar() {
     setIsReloading(true);
     window.location.reload();
   };
+    const hundleProfile = () =>{
+        navigate('/profile')
+    }
     return (
         <div >
             {navbarBublicationVisible && <div className="dark-overlay"></div>}
@@ -467,46 +470,34 @@ function Navbar() {
 
                         <ul className="menu-links">
 
-                            <li className="nav-link">
-                                <a href="#">
+                            <li className="nav-link" onClick={hundleProfile}>
                                     <i className='icon'><FaUserCog style={{fontSize: 30}} /></i>
                                     <span className="text">Compte</span>
-                                </a>
                             </li>
 
                             <li className="nav-link">
-                                <a href="#">
                                     <i className='bx bx-bar-chart-alt-2 icon'></i>
                                     <span className="text">Revenue</span>
-                                </a>
                             </li>
 
                             <li className="nav-link">
-                                <a href="#">
                                     <i className='bx bx-bell icon'></i>
                                     <span className="text">Notifications</span>
-                                </a>
                             </li>
 
                             <li className="nav-link">
-                                <a href="#">
                                     <i className='bx bx-pie-chart-alt icon'></i>
                                     <span className="text">Analytics</span>
-                                </a>
                             </li>
 
                             <li className="nav-link">
-                                <a href="#">
                                     <i className='bx bx-heart icon'></i>
                                     <span className="text">Likes</span>
-                                </a>
                             </li>
 
                             <li className="nav-link">
-                                <a href="#">
                                     <i className='bx bx-wallet icon'></i>
                                     <span className="text">Wallets</span>
-                                </a>
                             </li>
 
                         </ul>
--- frontend/chat-frontend/src/pages/Profile.js
+++ frontend/chat-frontend/src/pages/Profile.js
@@ -1,29 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './css/Profile.css';
+import { IoChevronBackSharp } from "react-icons/io5";
+import { FaPhone } from "react-icons/fa";
+import {CiEdit, CiUser} from "react-icons/ci";
 
 const Profile = () => {
+    const [userInfo, setUserInfo] = useState({});
+
+    useEffect(() => {
+        const loadUserInfo = async () => {
+            const storedUserInfo = localStorage.getItem('userInfo');
+            if (storedUserInfo) {
+                setUserInfo(JSON.parse(storedUserInfo));
+            }
+        };
+
+        loadUserInfo();
+    }, []);
+
     return (
         <div className="profile-container">
-            <header className="header">
-                <button className="back-button">&lt;</button>
-                <h1>Profil</h1>
-                <div className="header-spacer"></div>
+            <header className="header2">
+                <div className="header-content">
+                    <IoChevronBackSharp style={{ fontSize: 25 }} />
+                    <h1>Profil utilisateur</h1>
+                </div>
             </header>
             <div className="profile-info">
-                <img src="path_to_image.jpg" alt="Profile" className="profile-pic" />
+                <img
+                    src={userInfo.image_utilisateu || 'default_image_path.jpg'}
+                    alt="Profile"
+                    className="profile-pic"
+                />
                 <div className="profile-details">
                     <div className="profile-item">
-                        <span className="label">Nom</span>
-                        <span className="value">@D_wally09</span>
-                        <p className="note">Ce n’est pas votre nom d’utilisateur·ice ni votre code PIN. Ce nom sera visible par vos contacts WhatsApp.</p>
+                        <CiUser />
+                            <span className="label">Nom</span>
+                        <span className="value">{userInfo.nom_utilisateur || 'Nom non défini'}</span>
+                        <div className="note-container">
+                            <p className="note">Ce n’est pas votre nom d’utilisateur. Ce nom sera visible par vos contacts chater.</p>
+                            <CiEdit className="edit-icon" />
+                        </div>
+                    </div>
+                    <div className="profile-item">
+                        <CiUser />
+                        <span className="label">Prénom</span>
+                        <span className="value">{userInfo.prenom_utilisateur || 'Nom non défini'}</span>
+                        <div className="note-container">
+                            <p className="note">Ce n’est pas votre nom d’utilisateur. Ce nom sera visible par vos contacts chater.</p>
+                            <CiEdit className="edit-icon" />
+                        </div>
                     </div>
                     <div className="profile-item">
                         <span className="label">Infos</span>
                         <span className="value">En ligne le 07/03/2023</span>
                     </div>
                     <div className="profile-item">
+                        <FaPhone />
                         <span className="label">Téléphone</span>
-                        <span className="value">+225 0789817277</span>
+                        <span className="value">{userInfo.telephone || '+225 0789817277'}</span>
                     </div>
                 </div>
             </div>
