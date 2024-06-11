e83f2648384480e7bd78784c3ebede234e5a698e
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-06-11 06:30:47 +0000

    409

--- frontend/chat-frontend/src/index.js
+++ frontend/chat-frontend/src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import theme from "./theme";
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
 
-      <ChakraProvider theme={theme}>
+      <ChakraProvider theme={theme}>²
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <App />
       </ChakraProvider>
@@ -21,3 +22,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
