93cf7c04f106044997f397e4c3fbc2ba71d52a51
Alidouwarma09 <111253567+Alidouwarma09@users.noreply.github.com>
2024-06-27 14:34:48 +0000

    dsdsszxxxx

--- frontend/chat-frontend/src/pages/Userdetail.js
+++ frontend/chat-frontend/src/pages/Userdetail.js
@@ -1,18 +1,19 @@
-import React, {useEffect, useState} from 'react';
-import './css/userdetails.css'
+import React, { useEffect, useState } from 'react';
+import './css/userdetails.css';
 import axios from "axios";
-import {useNavigate, useParams} from "react-router-dom";
-import {Box} from "@mui/material";
-import {Spinner} from "@chakra-ui/react";
-import Icon from "antd/es/icon";
-import {IoReloadSharp} from "react-icons/io5";
-import {RiVerifiedBadgeFill} from "react-icons/ri";
+import { useNavigate, useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+import { Spinner } from "@chakra-ui/react";
+import { IoReloadSharp } from "react-icons/io5";
+import { RiVerifiedBadgeFill } from "react-icons/ri";
 import ModalImage from "react-modal-image";
-import {FaFacebookMessenger, FaLink} from "react-icons/fa";
+import { FaFacebookMessenger, FaLink } from "react-icons/fa";
+import {IoMdArrowRoundBack} from "react-icons/io";
 
-function Userdetail( ) {
+function Userdetail() {
     const { utilisateurId } = useParams();
     const [utilisateurs, setUtilisateurs] = useState(null);
+    const [isHeaderVisible, setIsHeaderVisible] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -21,8 +22,6 @@ function Userdetail( ) {
             navigate('/connexion');
         }
     }, [navigate]);
-
-
 
     useEffect(() => {
         const fetchUtilisateurs = async () => {
@@ -47,7 +46,24 @@ function Userdetail( ) {
         fetchUtilisateurs();
     }, [utilisateurId]);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 10) {
+                setIsHeaderVisible(false);
+            } else {
+                setIsHeaderVisible(true);
+            }
+        };
 
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    const handleBackss = () => {
+        navigate(-1);
+    };
     if (!utilisateurs) {
         return (
             <div className="loading-container">
@@ -60,91 +76,72 @@ function Userdetail( ) {
                         size="xl"
                         className="loading-container"
                     />
-                    <Icon as={IoReloadSharp} className="loading-icon" />
+                    <IoReloadSharp className="loading-icon" />
                 </Box>
             </div>
         );
     }
+
     return (
-        <div className="deatils-container">
-            <header className="profile__header" style={{backgroundImage:  `url(${utilisateurs.image_utilisateur})`, backgroundSize: "cover"}}>
-                <div className="profile__highlight__wrapper">
-                    Chater
+        <div className="details-container">
+           <span className="bouton-de-retour">
+                <IoMdArrowRoundBack size={30} onClick={handleBackss}  />
+           </span>
+            <div style={{ width: "100%"}}>
+                <header className="profile__header" style={{ backgroundImage: `url(${utilisateurs.image_utilisateur})`, backgroundSize: "cover" }}>
+                    <div className="profile__highlight__wrapper">
+                        Chater
+                    </div>
+                    <div className="profile__avatar">
+                        <ModalImage
+                            small={utilisateurs.image_utilisateur}
+                            large={utilisateurs.image_utilisateur}
+                            alt={`${utilisateurs.nom_utilisateur} ${utilisateurs.prenom_utilisateur} `}
+                            hideDownload={true}
+                            showRotate={false}
+                        />
+                    </div>
+                    <div className="profile__highlight__wrapper">
+                        Copyright
+                    </div>
+                </header>
+                <div className="profile__name">
+                    <h2>{utilisateurs.nom_utilisateur} {utilisateurs.prenom_utilisateur}<RiVerifiedBadgeFill style={{ color: "blue", fontSize: 20, marginLeft: 10 }} /></h2>
+                    <p>Compte professionnel/Musique</p>
                 </div>
-                <div className="profile__avatar" >
-                    <ModalImage
-                        small={utilisateurs.image_utilisateur}
-                        large={utilisateurs.image_utilisateur}
-                        alt={`${utilisateurs.nom_utilisateur} ${utilisateurs.prenom_utilisateur} `}
-                        hideDownload={true}
-                        showRotate={false}
-                    >
-                    </ModalImage>
-                </div>
-                <div className="profile__highlight__wrapper">
-                    Copyright
-                </div>
-            </header>
-
-
-
-            <div className="profile__name">
-                <h2>{utilisateurs.nom_utilisateur} {utilisateurs.prenom_utilisateur}<RiVerifiedBadgeFill style={{color: "blue", fontSize: 20, marginLeft: 10}} /></h2>
-                <p>Compte proffessionnel/Musique </p>
+                <ul className="social-links">
+                    <li>
+                        <span>
+                            <FaLink className="elements" />
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            <FaLink className="elements" />
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            <FaLink className="elements" />
+                        </span>
+                    </li>
+                    <li>
+                        <span>
+                            <FaLink className="elements" />
+                        </span>
+                    </li>
+                </ul>
+                <ul className="social-links2">
+                    <li>
+                        Message
+                        <FaFacebookMessenger className="elements" />
+                    </li>
+                </ul>
             </div>
-            <ul className="social-links">
-                <li>
-                    <span>
-                        <FaLink className="elements" />
-                    </span>
-                </li>
-                <li>
-                    <span>
-                        <FaLink className="elements" />
-                    </span>
-                </li>
-                <li>
-                    <span>
-                        <FaLink className="elements" />
-                    </span>
-                </li>
-                <li>
-                    <span>
-                        <FaLink className="elements" />
-                    </span>
-                </li>
-            </ul>
-
-            <ul className="social-links2">
-                <li>
-                    Message
-                    <FaFacebookMessenger className="elements" />
-                </li>
-            </ul>
-
-            <div id="tab1-content" className="tab-content tab-content--active" >
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
-                fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf <p>fdfrf rvr rvrg rgvr'gf rgvrg rg rg'gf rfgf </p>
+            <div id="tab1-content" className="tab-content tab-content--active">
+                afficher les publication de lutilisateur ici
             </div>
-
         </div>
-
     );
 }
 
